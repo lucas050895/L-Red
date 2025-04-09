02835f13c8c2995827d56b3c2d41fc0deafc9cde
Lucas Conde <lucasconde742@gmail.com>
2025-04-08 21:54:36 -0300

    presupuestador

---
+++ links/presupuesto.php
@@ -0,0 +1,34 @@
+<?php
+    include('../bd/conecxion.php');
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head> 
+    <!-- CHARSET -->
+    <meta charset="UTF-8">
+    <!-- IE-EDGE -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- VIEWPORT -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- DESCRIPTION -->
+    <meta name="description" content="L-Red">
+    <!-- AUTHOR -->
+    <meta name="author" content="Lucas Conde">
+    <!-- TITTLE -->
+    <title>Presupuesto || L-Red</title>
+    <!-- STYLES -->
+    <link rel="stylesheet" href="../css/presupuesto.css">
+    <!-- BOXICONS  -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <?php
+        include("../layout/nav.php");
+    ?>
+    <main>
+        
+    </main>
+    
+</body>
+</html>
