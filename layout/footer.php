185e4d8761b285c7574c515d1f5d114b5c2cad16
Lucas Conde <lucasconde742@gmail.com>
2025-05-01 20:09:04 -0300

    Nuevos estilos y agregando footer

---
+++ layout/footer.php
@@ -0,0 +1,7 @@
+<div id="footer">
+    <h3>Derechos Reservados © 2025</h3>
+    <a href="http://lucasconde.ddns.net/L-Red/links/login.php">
+        <i class="fa-solid fa-user"></i>
+        Iniciar Sesión
+    </a>
+</div>
