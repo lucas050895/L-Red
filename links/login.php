dd30a6d07b746b4d29e29e1e9b11a3fbc99415e0
Lucas Conde <lucasconde742@gmail.com>
2025-05-05 19:08:48 -0300

    Agregando ruta META

--- links/login.php
+++ links/login.php
@@ -25,8 +25,9 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+        include("../layout/meta.php");
+    ?>
     <title>Inciar Sesión || L-Red</title>
 
     <link rel="stylesheet" href="../css/login.css">
