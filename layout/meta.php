9b9565d093f518e91315b20f32616eea2ec335ae
Lucas Conde <lucasconde742@gmail.com>
2025-05-05 19:10:36 -0300

    Agregando archivo META

---
+++ layout/meta.php
@@ -0,0 +1,11 @@
+<meta charset="UTF-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<meta name="description" content="Este sitio esta diseñado para mostrar los trabajos de L-Red y además para administrar clientes y trabajos de la misma empresa">
+
+<meta name="keywords" content="website, L-Red, Servicio Técnico, Trabajos de red, Cámaras CCTV, Cámaras IP">
+
+<meta name="robots" content="index, follow">
+
+<meta name="author" content="Lucas Conde">
