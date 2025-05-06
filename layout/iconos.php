7d3b9babc13543ddc2016a300787d2119dec2bed
Lucas Conde <lucasconde742@gmail.com>
2025-05-06 17:58:20 -0300

    Agregando layout ICONOS

---
+++ layout/iconos.php
@@ -0,0 +1,6 @@
+    <!-- BOXICONS  -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    
+    <!-- FONT AWESOME -->   
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
