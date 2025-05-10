08e3ecb8e17748de519c6141f618163ef685dba0
Lucas Conde <lucasconde742@gmail.com>
2025-05-10 19:43:58 -0300

    Subiendo usuario

---
+++ php/subir_usuario.php
@@ -0,0 +1,27 @@
+<?php
+    include('../bd/conexion.php');
+
+    $usuario        =   $_POST['usuario'];
+    $password       =   $_POST['password'];
+    $nivel          =   $_POST['nivel'];
+
+
+    if(isset($_POST['submit'])){
+        $sql = "INSERT INTO usuarios(
+                                    usuario,
+                                    contraseña,
+                                    usuarios_nivel
+                                    )
+                            VALUES (
+                                        '{$usuario}',
+                                        '{$password}',
+                                        '{$nivel}'
+                                    )";
+        mysqli_query($conexion, $sql);
+    }
+
+    // Cerrar conexión a la base de datos
+    mysqli_close($conexion);
+
+    // Redirigir a la página de exitoso
+    header("Location: ../links/usuario.php");
