336ebde16abbc9dff49874e0491f474d6b573a27
Lucas Conde <lucasconde742@gmail.com>
2025-05-11 14:23:34 -0300

    Quitando la restriccion para ingresar

--- links/trabajo.php
+++ links/trabajo.php
@@ -11,34 +11,6 @@
     }else{
         header("Location: ../index.php");
     }
-
-
-    
-        // Verifica si el usuario está logueado.
-    if (!isset($_SESSION['usuario'])) {
-        // Si no está logueado, redirige a la página de inicio de sesión.
-        header("Location: login.php");
-        // exit();
-    }else {
-
-        $convertirUsuario = ucwords(strtolower($_SESSION['usuario']));
-
-        //sino, calculamos el tiempo transcurrido
-        $fechaGuardada = $_SESSION["ultimoAcceso"];
-
-        $ahora = date("Y-n-j H:i:s");
-        $tiempo_transcurrido = (strtotime($ahora)-strtotime($fechaGuardada));
-    
-        //comparamos el tiempo transcurrido
-        if($tiempo_transcurrido >= 60000) {
-        //si pasaron 10 minutos o más
-        session_destroy(); // destruyo la sesión
-        header("Location: login.php"); //envío al usuario a la pag. de autenticación
-        //sino, actualizo la fecha de la sesión
-        }else {
-        $_SESSION["ultimoAcceso"] = $ahora;
-        }
-    }
 ?>
 
 <!DOCTYPE html>
